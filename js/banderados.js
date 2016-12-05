24a21c82715f491948ddd71c140a04c34c65851c
Priscilla <priscillabuhrlerocha@gmail.com>
2016-12-05 19:45:43 -0300

    se agrega función generar código en banderados y funcion validar código para pasar a la página siguiente

--- js/banderados.js
+++ js/banderados.js
@@ -35,6 +35,18 @@ for (var i=0 ; i<paises.length ; ++i){
 $('#pais img').attr('src', pais.bandera);
 $('#prefijo').html(pais.prefijo);
 
+//crear codigo tres números js
+function generarCodigo(){
+	var codigo = (Math.floor(Math.random()*399)+1);
+	codigo = codigo.toString();
+	while(codigo.length < 3){
+		codigo = "0" + codigo;
+	}
+	localStorage.setItem('codigo', codigo);
+	return codigo;
+}
+// crear variable con el codigo aleatorio 
+var codig= generarCodigo();
 //validar numero y dar contraseña
 document.getElementById("inputbandera").addEventListener("input", function() {
    cajaTel = event.target;
@@ -42,12 +54,53 @@ document.getElementById("inputbandera").addEventListener("input", function() {
    mensaje = document.getElementById("telefonoOk");
        
    //Se va al span para mostrar mensaje
-    if (inputbandera.value.length > 8){
-      mensaje.innerText = "Maximo 8 numeros";
-   }if (inputbandera.value.length == 8){
-   	//convertir a número aleatorio
-      mensaje.innerText = "código 758";
-   }else {
+   
+   if (inputbandera.value.length > 8){
+      mensaje.innerText = "Maximo 8 numeros, teléfono no válido";
+  }else {
      mensaje.innerText = "Ingresar teléfono";
    }
 });
+//Se coloca el codigo aleatorio en otra función ya que en la anterior no la tomaba
+document.getElementById("inputbandera").addEventListener("input", function() {
+   cajaTel = event.target;
+   //identifica al span del html
+   mensaje = document.getElementById("telefonoOk");
+       if (inputbandera.value.length == 8){
+   	//codigo aleatorio
+      mensaje.innerText = "Tu código es:" + codig;
+     // alert(codig);
+ }
+});
+
+//validación del código
+
+
+	document.getElementById("codigoT").addEventListener("input", function() {
+		$('#btnCod').click(function(){
+		var cajacodIngresado = $("#codigoT").val();
+		if( cajacodIngresado !== codig){
+			corr.innerText = "código inválido";
+			//el false hace que el botón no cumpla su función
+			return false;
+			//toast elimina mensaje en tiempo determinado
+		}if( cajacodIngresado === codig){
+			corr.innerText = "código válido";
+		}
+		return true;
+	});
+	});
+
+
+/*function validarcodg(){
+	var codGenerado= generarCodigo();
+	  alert(generarCodigo());
+	var cajacodIngresado = $("#codigoT").val();
+		if( codigoIngresado !== codig ){
+			corr.innerText = "código inválido";
+			//toast elimina mensaje en tiempo determinado
+			Materialize.toast(mens, 3000);
+			return false;
+		}
+		validarcodg();
+}*/
